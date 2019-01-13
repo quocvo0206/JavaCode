2995fb00e70e864e9a0e7b2e9cf5c2624e6547ba
quocvo0206 <QuocVV@fsoft.com.vn>
2019-01-13 23:25:37 +0700

    11h2513012019

--- PtArray1D.java
+++ PtArray1D.java
@@ -1,5 +1,16 @@
 class PtArray1D{
 	public static void main(String[] ars){
-		System.out.println("HELLO WORLD!");
+		int s = 0;
+		int[] n = new int[5];
+		n[0] = 10;
+		n[1] = 20;
+		n[2] = 30;
+		n[3] = 40;
+		n[4] = 50;
+	for(int i = 0; i < n.length; i++){
+		System.out.print(" " + n[i] + " ");
+		s = s + n[i];
 	}
+	System.out.println("Sum is: "+ s);
+ 	}
 }
