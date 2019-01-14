bb73d853c2d0aafdc7c1fcc3ccf6043109ce9602
quocvo0206 <QuocVV@fsoft.com.vn>
2019-01-14 23:24:48 +0700

    11h2414012019

---
+++ PtArray2D.java
@@ -0,0 +1,10 @@
+class PtArray2D{
+	public static void main(String[] arr1){
+		String[][] arr = new String[1][4];
+		arr[0][0] ="   *    ";
+		arr[0][1] ="  *  *  ";
+		arr[0][2] =" *  *  * ";
+		arr[0][3] ="*  *  *  *";
+		System.out.print(arr[0][0]+"\n"+arr[0][1]+"\n"+arr[0][2]+"\n"+arr[0][3]);
+	}
+}
